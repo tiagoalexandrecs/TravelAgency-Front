37dbe422e65e060e27045f36989769ea98b210c7
Tiago <tiagoalex0202@gmail.com>
2023-05-28 19:50:58 -0300

    Estrtura geral finalizada e funcionando

--- src/Pages/HotelDetailsPage.js
+++ src/Pages/HotelDetailsPage.js
@@ -2,17 +2,42 @@ import styled from 'styled-components';
 import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { useNavigate , Link, useParams} from "react-router-dom";
+import detail1 from "./images/gabrielle-maurer-Q48pAVVON0o-unsplash.jpg"
+import detail2 from "./images/hans-vivek-RvfBEtCdYdA-unsplash.jpg"
+import detail3 from "./images/kirill-x_VOtnVJeGE-unsplash.jpg"
+import detail4 from "./images/louis-hansel-wVoP_Q2Bg_A-unsplash.jpg"
+import detail5 from "./images/masaaki-komori-iMbXscxYIFE-unsplash.jpg"
+import detail6 from "./images/paul-szewczyk-X2ef9uxU23E-unsplash.jpg"
+import detail7 from "./images/praveen-thirumurugan-nx05CDCrfzM-unsplash.jpg"
+import detail8 from "./images/katarzyna-pracuch-mglHaz5PjLg-unsplash.jpg"
+import detail9 from "./images/beazy-NDYJ1tjOuXI-unsplash.jpg"
+import detail10 from "./images/francesca-saraco-_dS27XGgRyQ-unsplash.jpg"
+import detail11 from "./images/mateo-fernandez-XTC538P_eWk-unsplash.jpg"
+import detail12 from "./images/jonathan-borba-00fCk2lZn1c-unsplash.jpg"
 
 export default function  HotelDetails(){
+
+    let images=[detail1, detail2, detail3, detail4, detail5, detail6, detail7, detail8, detail9, detail10, detail11, detail12]
 
     const {id}= useParams()
 
     const [details, setDetails]= useState([])
 
+    function getRandomItem(arr) {
+
+      const randomIndex = Math.floor(Math.random() * arr.length);
+  
+      const item = arr[randomIndex];
+  
+      return item;
+  }
+  
+
     useEffect(() => {
         axios
-          .get(`${process.env.REACT_APP_API_URL}/hotels/${id}`)
-          .then((res) => setDetails(res.data))
+          .get(`https://travelagency.onrender.com/hotel/${id}`)
+          .then((res) => {setDetails(res.data);
+            console.log(res.data)})
           .catch((err) => console.log(err.response.data));
       }, []);
 
@@ -20,17 +45,22 @@ export default function  HotelDetails(){
         <Container>
             <Header>
                   <div><h1>Viagens Alucinantes</h1></div>
-                  <Return><Link to={`/hotels/${details[0].city}`}><h1>VOLTAR</h1></Link></Return>
+                  <Return><Link to={`/hotels/${details[1]?.rows[0].id}`}><h2>VOLTAR</h2></Link></Return>
             </Header>
-            <Name>{details[0].name}</Name>
+            <Text>{details[0]?.name}</Text>
             <Images>
-                <Box></Box>
+                <Box1><img src={getRandomItem(images)}/> </Box1>
+                <Box><img src={getRandomItem(images)}/> </Box>
+                <Box><img src={getRandomItem(images)}/> </Box>
+                <Box><img src={getRandomItem(images)}/> </Box>
+                <Box><img src={getRandomItem(images)}/> </Box>
+                <Box><img src={getRandomItem(images)}/> </Box>
             </Images>
             <Details>
-                <Container2><Text>Características</Text>
-                <ul>{details[0].characteristics?.map((i) => <li>{i}</li>)}</ul></Container2>
-                <Container2><Text>Comodidades</Text>
-                <ul>{details[0].commodities?.map((i) => <li>{i}</li>)}</ul></Container2>
+                <Container2><Text2>Características:</Text2>
+                <ul>{details[0]?.characteristics?.map((i) => <Li>{i}</Li>)}</ul></Container2>
+                <Container2><Text2>Comodidades:</Text2>
+                <ul>{details[0]?.commodities?.map((i) => <Li>{i}</Li>)}</ul></Container2>
             </Details>
         </Container>
     )
@@ -42,11 +72,11 @@ const Header= styled.header `
 width: 100vw;
   height: 70px;
   position: fixed;
-  background: #ffffff;
+  background: #ffdf00;
   left: 0;
   top: 0;
   display: flex;
-  justify-content: left;
+  justify-content: center;
   align-items: center;
   z-index: 1;
   div {
@@ -60,55 +90,123 @@ width: 100vw;
       color: inherit;
     }
     h1 {
-      font-family: 'Architects Daughter', cursive;
       font-size: 40px;
+      font-family: 'Architects Daughter', cursive;
       font-weight: 700;
-      color: #6cc4b1;
+      color: #009c3b;
       text-align: center;
+      margin-left:370px;
+    }
+    h2{
+      font-size: 20px;
+      font-family: 'Architects Daughter', cursive;
+      font-weight: 700;
+      color: #009c3b;
+      text-align: center;
+      margin-left:200px;
     }
   }`;
 
   const Container= styled.div `
-background-color: lightblue;
+background-color: #002776;
+height: 80vw;
 display: flex;
 flex-direction: column;
 justify-content:center;
 align-items: center;`;
 
-const Name= styled.div `
-width: 100%;
-height: 100px;
-margin-top:20px;
-margin-bottom:20px;`;
-
-const Images= styled.div `
-width:100%;
-height: 300px;
-overflow-x: scroll;
+const Info= styled.div `
 display: flex;
-flex-direction: row;
-margin-bottom: 50px;
+flex-direction:column;
+justify-content:center;
+align-items:center;
 `;
 
-const Box= styled.div `
-height:150px;
-width:75px;
-img{
-    height:150px;
-    width:75px;
-}`
+const Text= styled.div `
+margin-bottom: 30px;
+font-family: 'Architects Daughter', cursive;
+ font-weight: 700;
+ font-size: 40px;
+ color: white;`;
 
 const Details= styled.div `
 display:flex;
-flex-direction:row;
-justify-content: space-between;
-align-items: center;`;
-
-const Container2= styled.div `
-background: white;`
+flex-direction: row;
+justify-content: space-evenly;
+margin-top:50px;
+width:90vw;
+margin-right:30px;
+`;
 
 const Return= styled.div `
-margin-left: 60%;`
+margin-left: 100px;`
 
-const Text= styled.div `
-`
+const Text2= styled.div `
+margin-bottom: 5px;
+font-family: 'Architects Daughter', cursive;
+ font-weight: 700;
+ font-size: 40px;
+ color: black;`;
+
+const Container2= styled.div `
+margin-top: 20px;
+height: 300px;
+width: 350px;
+border-radius: 8px;
+box-shadow: 10px 10px 5px lightblue;
+background: white;
+margin-left:20px;`;
+
+const Images= styled.div`
+overflow-x: scroll;
+display: flex;
+align-items:center;
+justify-content: center;
+width:90vw;
+height: 300px;
+background:white;
+border-radius: 8px;
+box-shadow: 10px 10px 5px lightblue;`;
+
+const Box= styled.div `
+height:250px;
+width:200px;
+margin-top:100px;
+img{
+    height:200px;
+    width:200px;
+    border: 1px solid #ccc;
+  padding: 10px;
+  text-align: center;
+  margin-right: 10px;
+  margin-bottom: 10px;
+};
+margin-bottom:110px;
+margin-left:50px;`;
+
+const Li= styled.li `
+font-family: 'Architects Daughter', cursive;
+ font-weight: 400;
+ color: black;
+ font-size: 20px;
+`;
+
+const Box1= styled.div `
+height:250px;
+width:200px;
+margin-top:100px;
+img{
+    height:200px;
+    width:200px;
+    border: 1px solid #ccc;
+  padding: 10px;
+  text-align: center;
+  margin-right: 10px;
+  margin-bottom: 10px;
+};
+margin-bottom:110px;
+margin-left:300px;`;
+
+
+
+
